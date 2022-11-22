fd403aefffb8db12d05271467d10bd0410460575
Veronika Kravchenko <aniregma@gmail.com>
2022-11-22 16:51:42 +0200

    fix

---
+++ js/hero-slider.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.hero-swiper-container', {
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+    slidesPerView: 1,
+    loop: true,
+    speed: 3000,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,16 +1,35 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector(".menu-open-btn"),
-    closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".mob-menu"),
-    body: document.querySelector("body"),
-  };
+const refs = {
+  openMenuBtn: document.querySelector(".menu-open-btn"),
+  closeMenuBtn: document.querySelector(".menu-close-btn"),
+  menu: document.querySelector(".mob-menu"),
+  menu: document.querySelector(".backdrop"),
+  body: document.querySelector("body"),
+};
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+refs.openMenuBtn.addEventListener("click", toggleMenu);
+refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
-})();
+export function toggleMenu() {
+  refs.menu.classList.toggle("is-hidden");
+  refs.body.classList.toggle("no-scroll");
+  refs.openMenuBtn.classList.toggle('is-hidden');
+  refs.closeMenuBtn.classList.toggle('is-hidden');
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const blockId = anchor.getAttribute('href');
+    document.querySelector('' + blockId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        closeMenu: closeMenu(e),
+    })
+  })
+}
+
+function closeMenu(e) {
+  if (e.target.closest('.mob-nav-item')) toggleMenu();
+}
