32145a31fadfe5a2ef782b7a27ada03e55e4e2b8
Veronika Kravchenko <aniregma@gmail.com>
2023-03-07 00:25:32 +0200

    fixes
    -fix form label position
    -bg-images
    -scripts

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,13 +1,15 @@
-export const heroSwiper = new Swiper('.hero-swiper-container', {
-    navigation: {
-        nextEl: '.swiper-btn-next',
-        prevEl: '.swiper-btn-prev',
-    },
-    slidesPerView: 1,
-    loop: true,
-    speed: 3000,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-});
+(() => {
+    new Swiper('.hero-swiper-container', {
+        navigation: {
+            nextEl: '.swiper-btn-next',
+            prevEl: '.swiper-btn-prev',
+        },
+        slidesPerView: 1,
+        loop: true,
+        speed: 3000,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    });
+})()
--- js/main.js
+++ js/main.js
@@ -1,48 +1,52 @@
-const refs = {
-  body: document.querySelector('body'),
-  openMenuBtn: document.querySelector('.menu-open-btn'),
-  closeMenuBtn: document.querySelector('.menu-close-btn'),
-  menu: document.querySelector('[data-mob-menu]'),
-  buttonTop: document.querySelector('[data-to-top]'),
-  anchors: document.querySelectorAll('a[href*="#"]'),
-};
-
-window.onscroll = () => showBtnToTop();
-
-refs.openMenuBtn.addEventListener('click', handleOpenCloseMenu);
-refs.closeMenuBtn.addEventListener('click', handleOpenCloseMenu);
-
-function handleOpenCloseMenu() {
-  toggleClass(refs.menu, 'is-hidden');
-  toggleClass(refs.body, 'no-scroll');
-  toggleClass(refs.openMenuBtn, 'is-hidden');
-  toggleClass(refs.closeMenuBtn, 'is-hidden');
-}
-
-function toggleClass(el, className) {
-  el.classList.toggle(className)
-}
-
-for (let anchor of refs.anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const blockId = anchor.getAttribute('href');
-    document.querySelector('' + blockId).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      closeMenu: closeMenu(e),
-    })
-  })
-}
-
-function closeMenu(e) {
-  if (e.target.closest('.mob-nav-item')) handleOpenCloseMenu();
-}
-
-function showBtnToTop() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    refs.buttonTop.classList.remove('hidden')
-  } else {
-    refs.buttonTop.classList.add('hidden')
+(() => {
+  const refs = {
+    body: document.querySelector('body'),
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menuBackdrop: document.querySelector('[data-mob-backdrop]'),
+    menu: document.querySelector('[data-mob-menu]'),
+    buttonTop: document.querySelector('[data-to-top]'),
+    anchors: document.querySelectorAll('a[href*="#"]'),
+  };
+  
+  window.onscroll = () => showBtnToTop();
+  
+  refs.openMenuBtn.addEventListener('click', handleOpenCloseMenu);
+  refs.closeMenuBtn.addEventListener('click', handleOpenCloseMenu);
+  
+  function handleOpenCloseMenu() {
+    toggleClass(refs.menuBackdrop, 'is-hidden');
+    toggleClass(refs.menu, 'is-hidden');
+    toggleClass(refs.body, 'no-scroll');
+    toggleClass(refs.openMenuBtn, 'is-hidden');
+    toggleClass(refs.closeMenuBtn, 'is-hidden');
   }
-}
+  
+  function toggleClass(el, className) {
+    el.classList.toggle(className)
+  }
+  
+  for (let anchor of refs.anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const blockId = anchor.getAttribute('href');
+      document.querySelector('' + blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        closeMenu: closeMenu(e),
+      })
+    })
+  }
+  
+  function closeMenu(e) {
+    if (e.target.closest('.mob-nav-item') || e.target.closest('.mob-connect')) handleOpenCloseMenu();
+  }
+  
+  function showBtnToTop() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      refs.buttonTop.classList.remove('hidden')
+    } else {
+      refs.buttonTop.classList.add('hidden')
+    }
+  }
+})()
